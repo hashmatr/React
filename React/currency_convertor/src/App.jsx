ebb9087772d9a7442429f2658c2d19ca5c5dc93d
Hashmat <hashmatraza.contact@gmail.com>
2025-07-20 17:58:37 +0500

    currency convertor

---
+++ React/currency_convertor/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './app.css'
+const App = () => {
+  return (
+   <h1 className='bg-red-700 mt-2 px-5 py-6 '>Hello World! I am going to make the currency Converter</h1>
+  )
+}
+
+export default App
